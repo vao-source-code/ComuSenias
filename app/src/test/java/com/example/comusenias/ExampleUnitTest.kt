d3043aef3179fdb7c67e3555e88921f95ab006e4
vorue <vorue@infoil.com.ar>
2023-09-02 11:19:59 -0300

    -test malo para probar el pull request

--- app/src/test/java/com/example/comusenias/ExampleUnitTest.kt
+++ app/src/test/java/com/example/comusenias/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.example.comusenias
 
+import org.junit.Test
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -7,5 +9,15 @@ package com.example.comusenias
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assert(4 == 2 + 2)
+    }
+
+    @Test
+    fun addition_isNotCorrect() {
+        assert(5 != 2 + 3)
+    }
 
 }
