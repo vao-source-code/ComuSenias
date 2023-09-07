668452fc1a7c84c82de03b129c6ecdd936820ed4
lanzarlosdados <zaratefabian@hotmail.com>
2023-09-07 15:32:37 -0300

    ui testing button app

---
+++ app/src/androidTest/java/com/example/comusenias/authentication/ButtonAppTest.kt
@@ -0,0 +1,67 @@
+package com.example.comusenias.authentication
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.unit.dp
+import com.example.comusenias.presentation.component.defaults.ButtonApp
+import org.junit.Rule
+import org.junit.Test
+
+class ButtonAppTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testButtonApp(){
+        composeTestRule.setContent {
+            ButtonApp(titleButton = "Iniciar sesión")
+        }
+        composeTestRule.onNodeWithTag("button").assertIsDisplayed()
+    }
+
+    @Test
+    fun testButtonAppHasClickAction(){
+        composeTestRule.setContent {
+            ButtonApp(
+                titleButton = "Iniciar sesión",
+            )
+        }
+        composeTestRule.onNodeWithTag("button").assertHasClickAction()
+    }
+    @Test
+    fun testButtonAppPerfomClick(){
+        composeTestRule.setContent {
+            ButtonApp(
+                titleButton = "Iniciar sesión"
+            )
+        }
+        composeTestRule.onNodeWithTag("button").performClick()
+    }
+
+    @Test
+    fun testButtonAppHeigth(){
+        composeTestRule.setContent {
+            ButtonApp(
+                titleButton = "Iniciar sesión"
+            )
+        }
+        composeTestRule.onNodeWithTag("button").assertHeightIsEqualTo(56.dp)
+    }
+
+    @Test
+    fun testButtonAppTitle(){
+        composeTestRule.setContent {
+            ButtonApp(
+                titleButton = "Iniciar sesión"
+            )
+        }
+        composeTestRule.onNodeWithText("Iniciar sesión").assertIsDisplayed()
+    }
+
+
+}
