062b99a06eee1b9fc7c3017d569870ab033d4386
Pablo Carballo <pablojavier.carballo@mercadolibre.com>
2023-09-10 19:06:06 -0300

    add test (#7)

--- app/src/main/java/com/example/comusenias/presentation/component/defaults/ImageDefault.kt
+++ app/src/main/java/com/example/comusenias/presentation/component/defaults/ImageDefault.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.Dp
 
 @Composable
@@ -18,5 +19,6 @@ fun GetImage(
         painter = painterResource(id = painter),
         contentDescription = contentDescription,
         Modifier.size(width, height)
+            .testTag("tagImage")
     )
 }
