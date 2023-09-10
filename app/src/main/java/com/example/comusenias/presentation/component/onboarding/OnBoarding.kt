d87f33546a879737b58c7d471c97aa330ff57499
Fabian Zarate <56480007+lanzarlosdados@users.noreply.github.com>
2023-09-10 12:12:31 -0300

    create package and class onboarding

---
+++ app/src/main/java/com/example/comusenias/presentation/component/onboarding/OnBoarding.kt
@@ -0,0 +1,4 @@
+package com.example.comusenias.presentation.component.onboarding
+
+class OnBoarding {
+}
---
+++ app/src/main/java/com/example/comusenias/presentation/screen/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,4 @@
+package com.example.comusenias.presentation.screen.onboarding
+
+class OnBoardingScreen {
+}
