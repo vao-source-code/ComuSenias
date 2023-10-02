1a07e6d554ea9f99b92ec3e2d11f605d998d7e1c
Fernando <fer_seba_18@yahoo.com.ar>
2023-10-02 13:30:04 -0300

    Notificaciones alpha

--- app/src/main/java/com/example/comusenias/ComuseniasApp.kt
+++ app/src/main/java/com/example/comusenias/ComuseniasApp.kt
@@ -1,8 +1,13 @@
 package com.example.comusenias
 
+import android.Manifest
 import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ComuseniasApp : Application() {
+
 }
