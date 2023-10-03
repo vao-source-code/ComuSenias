9d16cc205330540dd73934ac33633f3e56e9e4a8
Fernando <fer_seba_18@yahoo.com.ar>
2023-10-02 22:31:47 -0300

    Notificaciones alpha

--- app/src/main/java/com/example/comusenias/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/comusenias/presentation/activities/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.example.comusenias.presentation.activities
 
-import android.Manifest
-import android.app.NotificationChannel
-import android.app.PendingIntent
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.comusenias.R
 import com.example.comusenias.presentation.navigation.AppNavigation
 import com.example.comusenias.presentation.ui.theme.ComuSeniasTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,9 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
-    companion object{
-        const val MY_CHANNEL_ID = "MyChannel"
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,55 +32,6 @@ class MainActivity : ComponentActivity() {
                     AppNavigation(navController = navController)
                 }
             }
-        }
-        val myNotificationButton = findViewById<Button>(R.id.btnNotification)
-        createChannel()
-        myNotificationButton.setOnClickListener {
-            createSimpleNotification()
-        }
-    }
-
-    private fun createChannel() {
-        TODO("Not yet implemented")
-    }
-
-    private fun createSimpleNotification() {
-
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        val flag = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
-
-        var builder = NotificationCompat.Builder(this, MY_CHANNEL_ID)
-            .setSmallIcon(android.R.drawable.ic_delete)
-            .setContentTitle("Tus Notificaciones")
-            .setContentText("Notificaciones Version Alpha")
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText("Hola! Aca las notificaciones ")
-            )
-            .setContentIntent(pendingIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(this)) {
-
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            notify(1, builder.build())
         }
     }
 }
