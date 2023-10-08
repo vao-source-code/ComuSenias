e050a5d45144303d1fa002c7b26bcea3577ad966
Pablo Carballo <pablojavier.carballo@mercadolibre.com>
2023-10-08 19:28:37 -0300

    add test

--- app/src/androidTest/java/com/example/comusenias/domain/models/users/ChildrenModelTest.kt
+++ app/src/androidTest/java/com/example/comusenias/domain/models/users/ChildrenModelTest.kt
@@ -1,55 +1,71 @@
 package com.example.comusenias.domain.models.users
 
+import com.example.comusenias.domain.models.game.Level
+import com.example.comusenias.domain.models.observation.Observation
+import com.example.comusenias.presentation.component.home.StatusGame
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
-
 
 class ChildrenModelTest {
 
     private lateinit var userModel: UserModel
     private lateinit var specialist: SpecialistModel
     private lateinit var childrenModel: ChildrenModel
+    private lateinit var levels: List<Level>
+    private lateinit var observations: List<Observation>
 
     @Before
     fun setUp() {
+        levels = listOf(Level("1", StatusGame.COMPLETED, listOf(), listOf(), listOf()), Level("2", StatusGame.COMPLETED, listOf(), listOf(), listOf()))
+        observations = listOf(Observation("1", "1", "1"), Observation("2", "2", "2"))
+
+
         userModel = UserModel(
-            userName = "name",
-            password = "password",
-            email = "email",
+                userName = "name",
+                password = "password",
+                email = "email",
         )
 
         specialist = SpecialistModel(
-            userModel,
-            "12.12.2021",
-            "license",
-            "12.12.2021",
-            "speciality",
-            listOf()
+                userModel,
+                "12.12.2021",
+                "license",
+                "12.12.2021",
+                "speciality",
+                listOf()
         )
 
 
         childrenModel = ChildrenModel(
-            userModel = userModel,
-            date = "12.12.2021",
-            specialist = specialist,
-            isPremium = false,
-            levels = listOf(),
-            observation = listOf()
+                userModel = userModel,
+                date = "12.12.2021",
+                specialist = specialist,
+                isPremium = false,
+                levels = levels,
+                observation = observations
         )
     }
 
     @Test
-    fun `when`() {
-      val chidrenModel = ChildrenModel(
-          userModel = userModel,
-          date = "12.12.2021",
-          specialist = specialist,
-          isPremium = false,
-          levels = listOf(),
-          observation = listOf()
-      )
-
-        childrenModel.date = "12.12.2021"
+    fun whenCreateChildrenReturnEqualsDates() {
+        val date = "12.12.2021"
+        assertEquals(userModel, childrenModel.userModel)
+        assertEquals(date, childrenModel.date)
+        assertEquals(specialist, childrenModel.specialist)
+        assertEquals(false, childrenModel.isPremium)
+        assertEquals(levels, childrenModel.levels)
+        assertEquals(observations, childrenModel.observation)
     }
 
+    @Test
+    fun changeParametersChildrenReturnChanges() {
+        childrenModel.specialist = null
+        childrenModel.isPremium = true
+        childrenModel.observation = null
+
+        assertEquals(null, childrenModel.specialist)
+        assertEquals(true, childrenModel.isPremium)
+        assertEquals(null, childrenModel.observation)
+    }
 }
