9fde8c3aea50138d0335703aa1844d4d30a39765
Pablo Carballo <pablojavier.carballo@mercadolibre.com>
2024-02-13 23:03:24 -0300

    se agrega flipper y se modifica getleves para optimizar

--- app/src/main/java/com/example/comusenias/ComuseniasApp.kt
+++ app/src/main/java/com/example/comusenias/ComuseniasApp.kt
@@ -1,8 +1,40 @@
 package com.example.comusenias
 
 import android.app.Application
+import android.content.Context
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import okhttp3.OkHttpClient
 
 @HiltAndroidApp
 class ComuseniasApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initFlipper(this)
+        buildOkHttp(this)
+    }
+
+    private fun initFlipper(context : Context) {
+        SoLoader.init(this, false)
+        val networkPlugin = NetworkFlipperPlugin()
+        AndroidFlipperClient.getInstance(this).run {
+            addPlugin(networkPlugin)
+            addPlugin(InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()))
+            start()
+        }
+    }
+}
+private fun buildOkHttp(appContext: Context): OkHttpClient {
+    val plugin = AndroidFlipperClient
+        .getInstance(appContext)
+        .getPluginByClass(NetworkFlipperPlugin::class.java)
+    return OkHttpClient.Builder()
+        .addNetworkInterceptor(FlipperOkhttpInterceptor(plugin))
+        .build()
 }
